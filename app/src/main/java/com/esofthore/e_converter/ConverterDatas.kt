abaaef95acde128035a72bb7b1232a615ae8b392
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-01 13:49:34 -0300

    Creating the layout of the Dates screen

---
+++ app/src/main/java/com/esofthore/e_converter/ConverterDatas.kt
@@ -0,0 +1,11 @@
+package com.esofthore.e_converter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConverterDatas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_converter_datas)
+    }
+}
