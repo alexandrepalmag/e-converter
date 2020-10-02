319a4968059bf2769cd1b108fb2df8f83d417bf7
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-02 07:48:54 -0300

    Development of the temperature screen layout

---
+++ app/src/main/java/com/esofthore/e_converter/ConverterTemperature.kt
@@ -0,0 +1,11 @@
+package com.esofthore.e_converter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConverterTemperature : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_converter_temperature)
+    }
+}
