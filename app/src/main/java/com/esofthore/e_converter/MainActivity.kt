d200b2b532584b8d020ba90085857cbbb5a7194f
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-03 07:31:43 -0300

    Creating navigation for the ConvertLength screen

--- app/src/main/java/com/esofthore/e_converter/MainActivity.kt
+++ app/src/main/java/com/esofthore/e_converter/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.esofthore.e_converter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //loading components
+        val btnLength = findViewById<ImageButton>(R.id.btnLength)
+        btnLength.setOnClickListener {
+            val intent = Intent(this, ConverterLength::class.java)
+            startActivity(intent)
+        }
     }
 }
