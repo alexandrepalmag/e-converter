4c8d0820fc7665cec296ac32960b0573b903c0c0
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-04 04:07:39 -0300

    Creating navigation for the ConvertWeight screen.

--- app/src/main/java/com/esofthore/e_converter/ConverterWeight.kt
+++ app/src/main/java/com/esofthore/e_converter/ConverterWeight.kt
@@ -2,10 +2,22 @@ package com.esofthore.e_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class ConverterWeight : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_converter_weight)
+
+        //active "up navigation"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item?.itemId == android.R.id.home){
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
